250f8b7a6afbc6e45ff138955960fb1db09fa69d
aafagundes <73072881+aafagundes@users.noreply.github.com>
2021-01-30 21:51:06 -0300

    Delete tac3ex4.py

--- tac3ex4.py
+++
@@ -1,6 +0,0 @@
-#4
-# Escreva um código em Python que leia um número não determinado de valores
-# e calcule e escreva a média aritmética dos valores lidos, a quantidade de
-# valores positivos, a quantidade de valores negativos e o percentual
-# de valores negativos e positivos.
-
